288f1a1901f15ab372994eb20eb64ae60510cec3
Milena Strassburger <milenastrass@gmail.com>
2025-01-21 19:13:57 -0300

    Estudo de JavaScript

---
+++ scripts.js
@@ -0,0 +1,104 @@
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+const expenseList = document.querySelector("ul")
+const expenseTotal = document.querySelector("aside header h2")
+const expenseQuantity = document.querySelector("aside header p span")
+
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "")
+    value = Number(value / 100)
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+    return value
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        categoryId: category.value,
+        categoryName: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        createdAt: new Date(),
+    }
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+
+        const expenseIcon = document.createElement("img")
+        expenseIcon.setAttribute("src", `img/${newExpense.categoryId}.svg`)
+        expenseIcon.setAttribute("alt", newExpense.categoryName)
+
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info")
+
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
+
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.categoryName
+
+        expenseInfo.append(expenseName, expenseCategory)
+
+        const expenseAmount = document.createElement("span")
+        expenseAmount.classList.add("expense-amount")
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
+
+        const removeIcon = document.createElement("img")
+        removeIcon.classList.add("remove-icon")
+        removeIcon.setAttribute("src", "img/remove.svg")
+        removeIcon.setAttribute("alt", "remover")
+
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
+        expenseList.append(expenseItem)
+
+        updateTotals()
+    } catch (error) {
+        alert("Não foi possível atualizar a lista")
+        console.log(error)
+    }
+}
+
+function updateTotals() {
+    try {
+        const items = expenseList.children
+        expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+        let total = 0
+        for(let item = 0; item < items.length; item++) {
+            const itemAmount = items[item].querySelector(".expense-amount")
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",",".")
+            value = parseFloat(value)
+
+            if(isNaN(value)) {
+                return alert("Não foi possível calcular o total. O valor não parece ser um número.")
+            }
+
+            total += Number(value)
+        }
+
+        const symbolBRL = document.createElement("small")
+        symbolBRL.textContent = "R$"
+        total = formatCurrencyBRL(total).toUpperCase().replace("R$", "")
+        expenseTotal.innerHTML = ""
+        expenseTotal.append(symbolBRL, total)
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível atualizar os totais.")
+    }
+}
